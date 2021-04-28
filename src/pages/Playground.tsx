b61c9c998673133c0026efb41a3ca188dc4554e7
Stefan Plösser <stefan@nvon.com>
2021-04-28 12:46:09 +0200

    why is the count increasing by twos

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,24 +1,42 @@
-import React, { FunctionComponent } from "react";
-import BigPaper from "../components/BigPaper";
-import SayHello from "../components/SayHello/SayHelloComponent";
+import { Box, Button } from "@material-ui/core";
+import React, { FunctionComponent, useState } from "react";
 
-const BoxIt: FunctionComponent<{
-  title: string;
-}> = (props) => (
-  <BigPaper>
-    <h1>{props.title}</h1>
-    {props.children}
-  </BigPaper>
-);
+let numberOfCalls = 0;
+
+const LightBulb: FunctionComponent = () => {
+  const [isTurnedOn, setTurnedOn] = useState<boolean>(true);
+
+  numberOfCalls += 1;
+
+  console.log(`hi i am the lightbulb, been called ${numberOfCalls} times.`);
+
+  return (
+    <>
+      <Box
+        style={{
+          backgroundColor: isTurnedOn ? "yellow" : "black",
+          width: 100,
+          height: 100,
+          borderRadius: "50%",
+        }}
+      />
+      <Button
+        onClick={() => {
+          setTurnedOn((oldValue) => !oldValue);
+        }}
+      >
+        toggle light
+      </Button>
+    </>
+  );
+};
 
 const Playground: React.FunctionComponent = () => {
   console.log(`this is the playground`);
 
   return (
     <>
-      <BoxIt title="My Title">
-        <SayHello friends={[]} name="Stefan" />
-      </BoxIt>
+      <LightBulb />
     </>
   );
 };
